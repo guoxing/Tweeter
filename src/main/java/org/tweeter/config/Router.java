de6451af7227ad9d5aeb781981731609db1c481a
Marcel Puyat <marcelp@stanford.edu>
2015-04-02 21:37:17 -0700

    Add comments to Router module

--- src/main/java/org/tweeter/config/Router.java
+++ src/main/java/org/tweeter/config/Router.java
@@ -5,8 +5,28 @@ import java.util.Map;
 import org.tweeter.controllers.FriendshipsController;
 import org.tweeter.controllers.StatusesController;
 
+/**
+ * The router is in charge of mapping an HTTP-method & path pair (along with
+ * the parameters) to an appropriate controller's method.
+ * 
+ * It interfaces with the HTTPRequestHandler module, being passed in an HTTPRequest.
+ * It then unwraps the HTTPRequest to put together an Action, comprised of
+ * the http method, path, and the parameters. The http method and path together
+ * uniquely correspond to a controller's method.
+ * @author marcelpuyat
+ *
+ */
 public class Router {
     
+    /**
+     * Helper class that simply wraps together the fields from an HTTP request
+     * that are necessary to route a request to a controller's method. The
+     * httpMethod combined with the path make up what is referred to as an
+     * address. This is then used by the route method in a switch statement
+     * to take actions corresponding to the address.
+     * @author marcelpuyat
+     *
+     */
     private static class Action {
         private String httpMethod;
         private String path;
@@ -31,6 +51,14 @@ public class Router {
 //        
 //    }
     
+    /**
+     * Routes an httpMethod path pair to a controller's method, passing along
+     * the parameters.
+     * 
+     * The httpMethod, path, and parameter are passed into the method through a
+     * helper class (Action) to abstract away all the details of an HTTPRequest.
+     * @param action Holds the address (i.e. the httpMethod path pair) and the parameters
+     */
     private static void route(Action action) {
         Map<String, String> params = action.getParams();
         switch(action.getAddress()) {
@@ -54,6 +82,9 @@ public class Router {
                 return;
             case "GET /statuses/home_timeline.json":
                 StatusesController.getHomeTimeline(params);
+                return;
+            default:
+                // TODO: 404 error (invalid path/method pair)
                 return;
         }
     }
