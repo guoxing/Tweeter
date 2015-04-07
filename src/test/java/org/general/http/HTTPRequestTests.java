33a0fbfa013527a28d5f354d02678878c9d137cf
Guoxing Li <liguoxing64@gmail.com>
2015-04-07 13:41:08 -0700

    Add HTTP tests

---
+++ src/test/java/org/general/http/HTTPRequestTests.java
@@ -0,0 +1,64 @@
+package org.general.http;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Test;
+
+/**
+ * Test cases for HTTPRequest.
+ *
+ * @author Guoxing Li
+ *
+ */
+public class HTTPRequestTests {
+
+    private static final String DEFAULT_HOST = "www.google.com";
+    private static final String DEFAULT_CONTENT_TYPE = "text/html";
+    private static final String DEFAULT_VERSION = "HTTP/1.1";
+
+    private HTTPRequest generateRequest(String requestLine, String body)
+            throws Exception {
+        InputStream stream = new ByteArrayInputStream(
+                (requestLine + "Host: " + DEFAULT_HOST + " \n"
+                        + "Content-Type: " + DEFAULT_CONTENT_TYPE + "\n"
+                        + "Content-Length: " + body.length() + "\n\n" + body)
+                        .getBytes(StandardCharsets.UTF_8));
+        return new HTTPRequest(stream);
+    }
+
+    @Test
+    public void testGeneralParse() {
+        HTTPRequest req = null;
+        try {
+            req = generateRequest(HTTPRequest.Method.POST
+                    + " /path/to/resource?id=122 " + DEFAULT_VERSION + "\n",
+                    "id=123&name=Guoxing%20Li");
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Initialization fails.");
+        }
+        assertEquals(req.getMethod(), HTTPRequest.Method.POST);
+        assertEquals(req.getURI(), "/path/to/resource");
+        assertEquals(req.getHeaders().get("Host"), "www.google.com");
+        assertEquals(req.getQueryParams().get("id"), "123");
+        assertEquals(req.getQueryParams().get("name"), "Guoxing Li");
+    }
+
+    @Test
+    public void testMalformattedReq() {
+        try {
+            HTTPRequest req = generateRequest(HTTPRequest.Method.POST
+                    + "/path  " + DEFAULT_VERSION + "\n", "name=Guoxing Li");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        fail("Expected error not found.");
+    }
+
+}
---
+++ src/test/java/org/general/http/HTTPResponseTests.java
@@ -0,0 +1,129 @@
+package org.general.http;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.general.json.JSONList;
+import org.general.json.JSONMap;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test cases for HTTPResponse.
+ *
+ * @author Guoxing Li
+ *
+ */
+public class HTTPResponseTests {
+
+    private static final String TEST_HOST = "localhost";
+    private static final int TEST_PORT = 8889;
+
+    private ServerSocket ss;
+
+    @Before
+    public void setup() {
+        try {
+            ss = new ServerSocket(TEST_PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Exception found.");
+        }
+    }
+
+    @After
+    public void teardown() {
+        try {
+            ss.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Exception found.");
+        }
+    }
+
+    private JSONList genJSONList() {
+        JSONList list = new JSONList();
+        list.add(100);
+        list.add(200);
+        list.add(300);
+        return list;
+    }
+
+    @Test
+    public void testSendSuccess() {
+        try {
+            Socket clientSocket = new Socket(TEST_HOST, TEST_PORT);
+            Socket receivedSocket = ss.accept();
+            HTTPResponse res = new HTTPResponse(
+                    receivedSocket.getOutputStream(), "TestServer");
+            JSONList list = genJSONList();
+            res.setBody(list.toString());
+            res.sendSuccess(HTTPResponse.StatusCode.OK);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    clientSocket.getInputStream()));
+
+            String line = reader.readLine();
+            assertEquals(
+                    line,
+                    "HTTP/1.1 "
+                            + HTTPResponse.StatusCode.OK.getNum()
+                            + " "
+                            + HTTPResponse.StatusMessage
+                                    .get(HTTPResponse.StatusCode.OK));
+            while (!line.isEmpty()) {
+                line = reader.readLine();
+            }
+            line = reader.readLine();
+            assertEquals(line, list.toString());
+            receivedSocket.close();
+            clientSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Exception found.");
+        }
+    }
+
+    @Test
+    public void testSendError() {
+        try {
+            Socket clientSocket = new Socket(TEST_HOST, TEST_PORT);
+            Socket receivedSocket = ss.accept();
+            HTTPResponse res = new HTTPResponse(
+                    receivedSocket.getOutputStream(), "TestServer");
+            String errorMsg = "Params Error";
+            res.sendError(HTTPResponse.StatusCode.BAD_REQUEST, errorMsg);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    clientSocket.getInputStream()));
+
+            String line = reader.readLine();
+            assertEquals(
+                    line,
+                    "HTTP/1.1 "
+                            + HTTPResponse.StatusCode.BAD_REQUEST.getNum()
+                            + " "
+                            + HTTPResponse.StatusMessage
+                                    .get(HTTPResponse.StatusCode.BAD_REQUEST));
+            while (!line.isEmpty()) {
+                line = reader.readLine();
+            }
+            line = reader.readLine();
+            JSONMap map = new JSONMap();
+            map.put("ErrorMsg", errorMsg);
+            assertEquals(line, map.toString());
+            receivedSocket.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Exception found.");
+        }
+    }
+
+}
