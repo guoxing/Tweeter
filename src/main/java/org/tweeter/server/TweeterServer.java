d7bc68b6b191732720cff29a4c832634577f39fe
Guoxing Li <liguoxing64@gmail.com>
2015-04-10 14:46:46 -0700

    Spin up data module when server starts

--- src/main/java/org/tweeter/server/TweeterServer.java
+++ src/main/java/org/tweeter/server/TweeterServer.java
@@ -2,8 +2,11 @@ package org.tweeter.server;
 
 import java.io.IOException;
 
+import org.general.data.InvalidDataFormattingException;
 import org.general.http.HTTPServer;
 import org.tweeter.config.HTTPLayer;
+import org.tweeter.data.FriendshipData;
+import org.tweeter.data.StatusData;
 
 /**
  * Tweeter server class
@@ -15,8 +18,12 @@ public class TweeterServer {
 
     private static HTTPServer server;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException,
+            InvalidDataFormattingException {
         server = new HTTPServer("Tweeter/1.0", new HTTPLayer());
+        // spin up data modules
+        FriendshipData.getInstance();
+        StatusData.getInstance();
         try {
             server.start();
         } catch (IOException e) {
